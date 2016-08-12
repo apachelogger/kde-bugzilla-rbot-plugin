2ac435552c3cfef022710e4c277ba16513f752fa
Harald Sitter <sitter@kde.org>
2016-08-12 11:56:35 +0200

    try to avoid rake

--- test/test_run.rb
+++ test/test_run.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+
 # This is a fancy wrapper around test_helper to prevent the collector from
 # loading the helper twice as it would occur if we ran the helper directly.
 require_relative 'test_helper'
