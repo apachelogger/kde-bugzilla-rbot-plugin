1db5290b9d97f39ffaa3b1e30f029f8c8e6e3e22
Harald Sitter <sitter@kde.org>
2017-02-27 17:11:39 +0100

    move

--- test/test_run.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-
-# This is a fancy wrapper around test_helper to prevent the collector from
-# loading the helper twice as it would occur if we ran the helper directly.
-require(File.expand_path('test_helper', File.dirname(__FILE__)))
-
-Test::Unit::AutoRunner.run(true, File.absolute_path(__dir__))
